b85c4e6ba4398286e8c6ca04fbbc17ae9812db35
Simon Vreeman <simonvreeman@gmail.com>
2019-07-10 21:38:01 +0200

    Test

--- cloudflare.js
+++ cloudflare.js
@@ -1,2 +1,15 @@
 // <script src="https://vreeman.com/cloudflare.js" importance="high" async defer></script>
 console.log('https://vreeman.com/cloudflare.js is loaded.');
+
+callFunctionFromScript
+
+function callFunctionFromScript() {
+  var today = new Date();
+  var date = new Date();
+  var days = document.getElementById('days').value;
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  expires = date.toUTCString();
+  console.log(today);
+  console.log(date);
+  // document.cookie = "vreeman-js=" + today + "; expires=" + expires + "; path=/";
+}
